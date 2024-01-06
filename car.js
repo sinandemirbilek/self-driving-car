a74fb537a5cb9014952907aed91b04f426572638
Sinan Demirbilek <63739956+sinandemirbilek@users.noreply.github.com>
2024-01-06 12:24:49 +0300

    visual

--- car.js
+++ car.js
@@ -12,63 +12,72 @@ class Car{
         this.angle=0;
         this.damaged=false;
 
-        if(controlType!="DUMMY") {
+        this.useBrain=controlType=="AI";
+
+        if(controlType!="DUMMY"){
             this.sensor=new Sensor(this);
-            
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
         }
         this.controls=new Controls(controlType);
     }
 
     update(roadBorders,traffic){
-        if(!this.damaged) {
+        if(!this.damaged){
             this.#move();
             this.polygon=this.#createPolygon();
             this.damaged=this.#assessDamage(roadBorders,traffic);
         }
-        if(this.sensor) {
+        if(this.sensor){
             this.sensor.update(roadBorders,traffic);
+            const offsets=this.sensor.readings.map(
+                s=>s==null?0:1-s.offset
+            );
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward=outputs[0];
+                this.controls.left=outputs[1];
+                this.controls.right=outputs[2];
+                this.controls.reverse=outputs[3];
+            }
         }
     }
 
-    #assessDamage(roadBorders,traffic) {
-        for(let i=0;i<roadBorders.length;i++) {
-            if(polysIntersect(this.polygon,roadBorders[i])) {
-
+    #assessDamage(roadBorders,traffic){
+        for(let i=0;i<roadBorders.length;i++){
+            if(polysIntersect(this.polygon,roadBorders[i])){
                 return true;
             }
-            
         }
-        for(let i=0;i<traffic.length;i++) {
-            if(polysIntersect(this.polygon,traffic[i].polygon)) {
-
+        for(let i=0;i<traffic.length;i++){
+            if(polysIntersect(this.polygon,traffic[i].polygon)){
                 return true;
             }
-            
         }
-        
-        
         return false;
     }
 
-    #createPolygon() {
+    #createPolygon(){
         const points=[];
-        const rad = Math.hypot(this.width,this.height)/2;
-        const alpha = Math.atan2(this.width,this.height);
+        const rad=Math.hypot(this.width,this.height)/2;
+        const alpha=Math.atan2(this.width,this.height);
         points.push({
             x:this.x-Math.sin(this.angle-alpha)*rad,
-            y:this.y-Math.cos(this.angle-alpha)*rad,
+            y:this.y-Math.cos(this.angle-alpha)*rad
         });
         points.push({
             x:this.x-Math.sin(this.angle+alpha)*rad,
-            y:this.y-Math.cos(this.angle+alpha)*rad,
+            y:this.y-Math.cos(this.angle+alpha)*rad
         });
         points.push({
             x:this.x-Math.sin(Math.PI+this.angle-alpha)*rad,
-            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle-alpha)*rad
         });
         points.push({
             x:this.x-Math.sin(Math.PI+this.angle+alpha)*rad,
-            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad,
+            y:this.y-Math.cos(Math.PI+this.angle+alpha)*rad
         });
         return points;
     }
@@ -112,20 +121,20 @@ class Car{
         this.y-=Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx,color){
-        if(this.damaged) {
+    draw(ctx,color,drawSensor=false){
+        if(this.damaged){
             ctx.fillStyle="gray";
         }else{
             ctx.fillStyle=color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
-        for(let i=1;i<this.polygon.length;i++) {
+        for(let i=1;i<this.polygon.length;i++){
             ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
         }
         ctx.fill();
 
-        if(this.sensor) {
+        if(this.sensor && drawSensor){
             this.sensor.draw(ctx);
         }
     }
